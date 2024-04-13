0db9ed4b39f7058a5a8286af571834c242af94e2
Ariana Falisya <155871976+ariwooze@users.noreply.github.com>
2024-04-13 23:45:50 +0800

    Edited HTML & CSS
    I'm still working on the tutorial code but there are errors that prevent it from working. But you can just look at the html part first. fyi: run the code from base.html not from main.py

--- website/auth.py
+++ website/auth.py
@@ -24,8 +24,6 @@ def profile():
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
     return render_template("login.html", boolean=True)
 
 
@@ -51,7 +49,6 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            # add user to database
             flash('Account created!', category='success')
 
     return render_template("sign_up.html")
