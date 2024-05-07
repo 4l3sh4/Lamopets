e393ed616383b3595fe07d7cdffa9cb553e9e047
amirah <abbyazlan110@gmail.com>
2024-05-07 15:38:17 +0800

    current adopt layout

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    currency_balance = db.Column(db.Integer, default=1000)
 
 
 class RegisterForm(FlaskForm): 
--- static/js/main.js
+++ static/js/main.js
@@ -24,3 +24,83 @@ window.addEventListener('resize', function() {
         document.getElementById("navicon").style.display = "block"; 
     }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll('.store');
+    const navLinks = document.querySelectorAll('.nav-store a');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const activeLink = document.querySelector(`.nav-store a[href="#${entry.target.id}"]`);
+                navLinks.forEach(link => link.classList.remove('active'));
+                activeLink.classList.add('active');
+            }
+        });
+    }, { rootMargin: '0px', threshold: 0.05 });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll('.adopt');
+    const navLinks = document.querySelectorAll('.nav-adopt a');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const activeLink = document.querySelector(`.nav-adopt a[href="#${entry.target.id}"]`);
+                navLinks.forEach(link => link.classList.remove('active'));
+                activeLink.classList.add('active');
+            }
+        });
+    }, { rootMargin: '0px', threshold: 0.5 });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const colourButtons = document.querySelectorAll('.colour-options');
+    const item = document.getElementById('head1');
+    colourButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const colour = this.getAttribute('data-filter');
+            switch(colour) {
+                case 'black':
+                    item.style.filter = 'grayscale(50%) brightness(40%) saturate(400%)';
+                    break;
+                case 'blue':
+                    item.style.filter = 'saturate(150%) sepia(70%) hue-rotate(180deg)';
+                    break;
+                case 'green':
+                    item.style.filter = 'saturate(200%) sepia(50%) hue-rotate(90deg)';
+                    break;
+            }
+        });
+    });
+}); 
+
+document.addEventListener('DOMContentLoaded', function() {
+    var priceButtons = document.querySelectorAll('.price');
+    var popup = document.getElementById('purchase-popup');
+
+    priceButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            popup.style.display = 'block';
+        });
+    });
+
+    window.onclick = function(event) {
+        if (event.target == popup) {
+            popup.style.display = "none";
+        }
+    }
+
+    document.getElementById('noButton').addEventListener('click', function() {
+        popup.style.display = 'none';
+    });
+});
