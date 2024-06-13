4e0cdc3cbfaa8edf53ca586f92709c96646707ee
amirah <abbyazlan110@gmail.com>
2024-06-13 10:58:35 +0800

    more fixes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for, redirect, request, abort, jso
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, DecimalField, SubmitField, SelectField
+from wtforms import StringField, PasswordField, DecimalField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError, DataRequired, EqualTo
 from flask_bcrypt import Bcrypt
 from datetime import datetime, timedelta
